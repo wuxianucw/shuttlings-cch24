bf50c044efe6c792feb1ba46b8fb373bc2c02134
无限UCW <x-wuxian@qq.com>
2024-12-03 02:27:29 +0800

    challenge -1

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use axum::{body::Body, http::{header::LOCATION, StatusCode}, response::Response, routing::get, Router};
+
+async fn hello_bird() -> &'static str {
+    "Hello, bird!"
+}
+
+async fn seek() -> Response {
+    Response::builder()
+        .status(StatusCode::FOUND)
+        .header(LOCATION, "https://www.youtube.com/watch?v=9Gc4QTqslN4")
+        .body(Body::empty())
+        .unwrap()
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(hello_bird))
+        .route("/-1/seek", get(seek));
+
+    Ok(router.into())
+}
